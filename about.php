2c0f01d7520c88d6f1b9ddeab61a30692f5c5787
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-11-06 14:00:26 -0600

    Update about.php

--- about.php
+++ about.php
@@ -14,7 +14,7 @@
     }
 
     function goBackHome() {
-        window.location.href = "index.html"; // Redirects to the homepage
+        window.location.href = "index.php"; // Redirects to the homepage
     }
     </script>
 
